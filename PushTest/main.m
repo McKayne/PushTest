e2a0386b6e24fe60d62f0faf66d5c526a85405ff
WagnerAutoPulkovo <wagnerautopulkovo@gmail.com>
2019-05-03 18:52:22 +0300

    May 3

--- PushTest/main.m
+++ PushTest/main.m
@@ -7,11 +7,23 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "NWPusher.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        
+        NSURL *url = [[NSURL alloc] initWithString:@"file:///Users/admin/Downloads/PerfectTemplate/demoAppCert.p12"];
+        
+        NSData *pkcs12 = [NSData dataWithContentsOfURL:url];
+        NSError *error = nil;
+        NWPusher *pusher = [NWPusher connectWithPKCS12Data:pkcs12 password:@"audidemo" environment:NWEnvironmentSandbox error:&error];
+        if (pusher) {
+            NSLog(@"Connected to APNs");
+        } else {
+            NSLog(@"Unable to connect: %@", error);
+        }
     }
     return 0;
 }
