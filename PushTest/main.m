2d11b29238cfefd43b431f9e95ab6539089f1bc6
WagnerAutoPulkovo <wagnerautopulkovo@gmail.com>
2019-05-03 18:44:00 +0300

    Initial Commit

---
+++ PushTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PushTest
+//
+//  Created by для интернета on 03/05/2019.
+//  Copyright © 2019 Nikolay Taran. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
